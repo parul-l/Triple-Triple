3d2a7f963df70e10e9b1e4d7194d5b5d69b05c9e
Parul <parul.laul@gmail.com>
2016-10-10 19:48:19 -0400

    formatting

--- triple_triple/data_generators/player_bio_data_webscraping.py
+++ triple_triple/data_generators/player_bio_data_webscraping.py
@@ -51,14 +51,14 @@ def player_bio_scraped_df():
 
                 try:
                     temp_list= [
-                        remove_unwanted_char(name_cell[0].text),    # name
-                        int(cells[0].text)-1,                       # from
-                        int(cells[1].text)-1,                       # to
-                        cells[2].text,                              # position
-                        cells[3].text,                              # height
-                        int(cells[4].text),                         # weight
-                        datetime.strptime(cells[5].text, '%B %d, %Y')
-                    ]                                               # birthday
+                        remove_unwanted_char(name_cell[0].text),      # name
+                        int(cells[0].text)-1,                         # from
+                        int(cells[1].text)-1,                         # to
+                        cells[2].text,                                # position
+                        cells[3].text,                                # height
+                        int(cells[4].text),                           # weight
+                        datetime.strptime(cells[5].text, '%B %d, %Y') # birthday
+                    ]                                                 
                     player_bio_info.append(temp_list)
 
                 except:
@@ -91,49 +91,49 @@ def add_ids_to_player_bio(df_player_bio, player_ids, current_year=2016):
     multiple_matches = []
     for_loop_not_working = {}
 
-    for key, value in player_ids.iteritems():
+    for playerid, name_year in player_ids.items():
         try:
-            idx_player_year = df_player_bio.loc[
-                                (df_player_bio['Player Name']==value[1]) & (df_player_bio['From']==value[2])].index
+            idx_player_year = df_player_bio.loc[\
+                                (df_player_bio['Player Name'] == name_year[1]) & (df_player_bio['From'] == name_year[2])].index
 
             # unique player names and years
             if len(idx_player_year) == 1:
-                id_index[idx_player_year] = key
+                id_index[idx_player_year] = playerid
 
             # player not found based on name and year
             elif len(idx_player_year) == 0:
 
                 # find index only based on player and not year
                 idx_player= df_player_bio.loc[
-                                (df_player_bio['Player Name']==value[1])].index
+                                (df_player_bio['Player Name'] == name_year[1])].index
 
                 # if there is only one match, add it to the index list
                 if len(idx_player) == 1:
-                    id_index[idx_player] = key
+                    id_index[idx_player] = playerid
 
                     # if there are 0 matches or more than one match
                     # check if the player's year is current year
                     # since current year 'from' isn't on basketball-reference?
 
-                elif value[2] == current_year:
+                elif name_year[2] == current_year:
                     # add it to no_match based on current year
-                    # key, name, from, to, position, height, weight, bday
+                    # playerid, name, from, to, position, height, weight, bday
                     no_match_idx_current_yr.append(
-                        [key, value[1], value[2], value[3],'NaN', 'NaN', 0, 0]
+                        [playerid, name_year[1], name_year[2], name_year[3],'NaN', 'NaN', 0, 0]
                         )
                 else:
                     # everything else if:
                     # 0 hits with name and year
                     # no unique hit or current year hit
-                    # key, name, from, to, position, height, weight, bday
+                    # playerid, name, from, to, position, height, weight, bday
                     no_match_idx_player_year_else.append(
-                        [key, value[1], value[2], value[3], 'NaN', 'NaN', 0, 0]
+                        [playerid, name_year[1], name_year[2], name_year[3], 'NaN', 'NaN', 0, 0]
                         )
             else:
                 # players with multiple hits for both name and year
-                multiple_matches.append((idx, key, value))
+                multiple_matches.append((idx, playerid, name_year))
         except:
-            for_loop_not_working[key] = value
+            for_loop_not_working[playerid] = name_year
 
     # add player_ids to DataFrame
     df_player_bio.insert(0, 'Player_ID', id_index)
@@ -154,9 +154,9 @@ def add_ids_to_player_bio(df_player_bio, player_ids, current_year=2016):
     df_current_year = pd.DataFrame(no_match_idx_current_yr,
                                    columns=headers_current_yr)
 
-    return df_player_bio.append(df_current_year, ignore_index =True)
+    return df_player_bio.append(df_current_year, ignore_index=True)
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     df_player_bio =  player_bio_scraped_df()
     df_player_bio = add_ids_to_player_bio(df_player_bio, player_ids,

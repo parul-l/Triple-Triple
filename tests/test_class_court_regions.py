89cdf4370e6cd708222a0e8b4866aef0d03a80fa
Parul <parul.laul@gmail.com>
2017-06-04 15:39:25 -0400

    Add more test files

---
+++ tests/test_class_court_regions.py
@@ -0,0 +1,35 @@
+import mock
+import unittest
+import triple_triple.class_court_regions as ccr
+
+
+class TestClassCourtRegions(unittest.TestCase):
+    """Tests for class_court_regions.py"""
+
+    def test_get_polygon_perimeter_out_of_bounds(self):
+        polygon_perimeter_l = ccr.get_polygon_perimeter('left')
+        polygon_perimeter_r = ccr.get_polygon_perimeter('right')
+
+        out_of_bounds_points = [
+            (0, -1),
+            (-1, 0),
+            (99, 10),
+            (10, 99),
+            (47, 51),
+            (47, -1),
+            (0, 0)
+        ]
+        for polygon_perimeter in polygon_perimeter_l, polygon_perimeter_r:
+            for x, y in out_of_bounds_points:
+                self.assertFalse(polygon_perimeter(x, y))
+
+        self.assertTrue(polygon_perimeter_l(46, 25))
+        self.assertTrue(polygon_perimeter_r(48, 25))
+        # polygon_perimeter_mock = mock.Mock()
+        # method_to_mock = ('triple_triple.class_court_regions.polygon_perimeter')
+        # with mock.patch(method_to_mock, polygon_perimeter_mock):
+        #     ccr.get_polygon_perimeter('right')
+        #     polygon_perimeter_mock.assert_called_once_with()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_play_animation.py
@@ -0,0 +1,40 @@
+import unittest
+
+import mock
+import numpy as np
+import pandas as pd
+
+import triple_triple.play_animation as pa
+
+
+class TestPlayAnimation(unittest.TestCase):
+    """Tests for play_animation.py"""
+
+    def test_get_fixedtime(self):
+        headers = ['game_clock', 'period', 'team_id']
+        game_clock = np.tile(np.arange(14, -1, -1), 2)
+        # shot_clock = np.arange(10, 7.5, -0.25).repeat(3)
+        period = np.array([1, 2]).repeat(15)
+        team_ids = np.tile([100, 100, 100, 100, 222, 222], 5)
+
+        mock_df = pd.DataFrame(
+            data=np.array([game_clock, period, team_ids]).T,
+            columns=headers
+        )
+
+        df = pa.get_fixedtime_df(
+            period=1,
+            time_start=11,
+            time_end=9,
+            dataframe=mock_df
+        )
+        self.assertEqual(df.shape[0], 3)
+        self.assertEqual(df.team_id.values.tolist(), [100, 222, 222])
+
+        np.testing.assert_array_equal(df.period.values, 1)
+        np.testing.assert_array_equal(9 <= df.game_clock.values, True)
+        np.testing.assert_array_equal(df.game_clock.values <= 11, True)
+
+
+if __name__ == '__main__':
+    unittest.main()

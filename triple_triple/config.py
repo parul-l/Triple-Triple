b791d4d8d62984a2b301e751c01a8875d3abf50b
Parul <parul.laul@gmail.com>
2019-04-24 15:05:52 -0400

    Added aws details to config file

--- triple_triple/config.py
+++ triple_triple/config.py
@@ -1,7 +1,14 @@
 import os
 
+# [directories]
+
 MODULE_HOME = os.path.dirname(__file__)
 
 DATASETS_DIR = os.path.join(MODULE_HOME, 'data')
 IMG_DIR = os.path.join(MODULE_HOME, 'img')
 MOCK_DATASETS_DIR = os.path.join(MODULE_HOME, 'tests', 'fixtures')
+
+# [aws]
+AWS_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_PASSWORD = os.environ.get('AWS_SECRET_ACCESS_KEY')
+ATHENA_OUTPUT = os.environ.get('AWS_ATHENA_OUTPUT_DIR')

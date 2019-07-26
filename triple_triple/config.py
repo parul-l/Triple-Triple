59d3508ff342cdc93f62932abb0fe02fef66e824
Parul <parul.laul@gmail.com>
2019-07-26 16:34:23 -0400

    Added sqlpath and athena info

--- triple_triple/config.py
+++ triple_triple/config.py
@@ -6,9 +6,11 @@ MODULE_HOME = os.path.dirname(__file__)
 
 DATASETS_DIR = os.path.join(MODULE_HOME, 'data')
 IMG_DIR = os.path.join(MODULE_HOME, 'img')
+SQL_DIR = os.path.join(MODULE_HOME, 'sql')
 MOCK_DATASETS_DIR = os.path.join(MODULE_HOME, 'tests', 'fixtures')
 
 # [aws]
 AWS_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_PASSWORD = os.environ.get('AWS_SECRET_ACCESS_KEY')
-ATHENA_OUTPUT = os.environ.get('AWS_ATHENA_OUTPUT_DIR')
+# ATHENA_OUTPUT = os.environ.get('AWS_ATHENA_OUTPUT_DIR')
+ATHENA_OUTPUT = 'aws-athena-query-results-401845286067-us-east-1'
